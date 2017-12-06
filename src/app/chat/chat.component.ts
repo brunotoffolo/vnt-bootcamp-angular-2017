45064300685cccc3a19bbee0a206c0ee6ec7af0b
Bruno Toffolo <brunotoffolo@gmail.com>
2017-12-06 19:38:47 -0200

    Use Rx.ReplayService to keep message history
    Implement a ReplaySubject using Rx library to keep track of the message
    history. This allows the ChatService to keep all received messages in a
    subject which can then stream them to any connected observable (such as
    any new chat client).
    
    This implementation uses the ReplaySubject to allow new clients to have
    access to the chat history (read messages which were sent before it has
    joined the chat). If clients should not have such privilege (e.g. read
    only messages which were sent after they have joined), the subject can
    then be replaced with an Rx.PublishSubject object.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -8,7 +8,7 @@ import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChi
 })
 export class ChatComponent implements OnInit, AfterViewChecked, AfterViewInit {
 
-  private _messageList: any[];
+  private _messageList: any[] = [];
   private _unsentMessage: string = '';
 
   readonly MAX_CHARACTER_COUNT: number = 300;
@@ -18,35 +18,35 @@ export class ChatComponent implements OnInit, AfterViewChecked, AfterViewInit {
   constructor (private _chatService: ChatService) { }
 
   ngOnInit() {
-    this._chatService._backend.on(
-      'messages',
+    this._chatService.subscribeToNewMessages(
       message => this._messageList.push(message)
     );
-    this._messageList = this._chatService.getMessages();
   }
-  
-    public sendMessage(): void {
-      this._chatService.sendMessage(this._unsentMessage);
-      this._unsentMessage = '';
-    }
 
-    public ngAfterViewChecked(): void {
-        this.scrollToLatestMessage();
-    }
+  public sendMessage(): void {
+    this._chatService.sendMessage(this._unsentMessage);
+    this._unsentMessage = '';
+  }
 
-    public ngAfterViewInit(): void {
+  public ngAfterViewChecked(): void {
       this.scrollToLatestMessage();
-    }
+  }
 
-    private scrollToLatestMessage(): void {
-      this._autoScrollingArea.nativeElement.scrollTop =
-        this._autoScrollingArea.nativeElement.scrollHeight;
-    }
+  public ngAfterViewInit(): void {
+    this.scrollToLatestMessage();
+  }
 
-    private onKeyPressed(event: KeyboardEvent): void {
-      if (event.key === 'Enter' && event.shiftKey !== true) {
-        this.sendMessage();
-      }
+  private scrollToLatestMessage(): void {
+    this._autoScrollingArea.nativeElement.scrollTop =
+      this._autoScrollingArea.nativeElement.scrollHeight;
+  }
+
+  private onKeyPressed(event: KeyboardEvent): void {
+    if (event.key === 'Enter' &&
+        event.shiftKey !== true &&
+        this._unsentMessage.trim().length > 0) {
+      this.sendMessage();
     }
+  }
 
 }
--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import { UserService } from '../user.service';
+import { ReplaySubject } from 'rxjs/Rx';
 import * as io from 'socket.io-client';
 
 @Injectable()
 export class ChatService {
 
-  private _messages: any[];
-  public _backend: any;
+  private _backend: any;
+  private _messageList$: ReplaySubject<any>;
 
   readonly CHAT_BACKEND_URL = 'http://localhost:3000/';
 
   constructor(private _userService: UserService) {
+    this._messageList$ = new ReplaySubject();
     this._backend = io(this.CHAT_BACKEND_URL);
-    this._messages = [];
+    this._backend.on(
+      'messages',
+      message => {
+        this._messageList$.next(message);
+      }
+    );
   }
 
-  public getMessages(): any[] {
-    return this._messages;
+  public subscribeToNewMessages(callback: any): void {
+    this._messageList$.subscribe(callback);
   }
 
   public sendMessage(messageText: any): void {
